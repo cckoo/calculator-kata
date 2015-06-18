194a6505053fcd4e5d97ae39de4aaf73ae1182e3
cckoo <595743688@qq.com>
2015-06-18 14:01:27 +0800

    commit8
    third test succeed

--- Calculator.php
+++ Calculator.php
@@ -1,6 +1,10 @@
 <?php
 class Calculator {
     public function add($stringNum) {
+        if (strpos($stringNum, ',')) {
+            $nums = explode(',', $stringNum);
+            return $nums[0] + $nums[1];
+        }
         if ($stringNum) {
             return $stringNum;
         }
