befc4d4b11e471618b8b4ecc424ba450faa64a97
cckoo <595743688@qq.com>
2015-06-18 14:05:34 +0800

    commit9
    refactor code
    add private method _getCount and _getNums
    add a global variable DELIMITER

--- Calculator.php
+++ Calculator.php
@@ -1,13 +1,20 @@
 <?php
+
+define("DELIMITER", ',');
+
 class Calculator {
     public function add($stringNum) {
-        if (strpos($stringNum, ',')) {
-            $nums = explode(',', $stringNum);
-            return $nums[0] + $nums[1];
+        if (strpos($stringNum, DELIMITER)) {
         }
         if ($stringNum) {
             return $stringNum;
         }
         return 0;
+    }
+
+        return explode(DELIMITER, $stringNum);
+    }
+
+        return $nums[0] + $nums[1];
     }
 }
