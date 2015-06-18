c549a42714b59c72c2f1009e008e5ead27dc6df7
cckoo <595743688@qq.com>
2015-06-18 14:06:27 +0800

    commint10
    refactor two private method _getNums and _getCount

--- Calculator.php
+++ Calculator.php
@@ -5,6 +5,8 @@ define("DELIMITER", ',');
 class Calculator {
     public function add($stringNum) {
         if (strpos($stringNum, DELIMITER)) {
+            $nums = $this->_getNums($stringNum);
+            return $this->_getCount($nums);
         }
         if ($stringNum) {
             return $stringNum;
@@ -12,9 +14,11 @@ class Calculator {
         return 0;
     }
 
+    private function _getNums($stringNum) {
         return explode(DELIMITER, $stringNum);
     }
 
+    private function _getCount($nums) {
         return $nums[0] + $nums[1];
     }
 }
